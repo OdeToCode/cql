7d60f62e54605d253e9f214fc255fc11ebb8ffc2
Scott Allen <scott@OdeToCode.com>
2017-09-08 20:26:12 -0400

    dump listener

--- experiment/Program.cs
+++ experiment/Program.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 
 namespace Experiment
 {
@@ -7,6 +8,24 @@ namespace Experiment
     public class Program
     {
         public static void Main()
+        {
+           RunArrayInit();
+           RunHello();
+        }
+
+        static void RunHello()
+        {
+            var reader = new StringReader("hello a\rhello b");
+            var input = new AntlrInputStream(reader);
+            var lexer = new helloLexer(input);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new helloParser(tokens);
+            var walker = new ParseTreeWalker();
+            var listener = new DumpHelloListener();
+            walker.Walk(listener, parser.r());
+        }
+
+        static void RunArrayInit()
         {
             var reader = new StringReader("{1, 2, { 3, 4}, 3, 4}");
             var input = new AntlrInputStream(reader);
